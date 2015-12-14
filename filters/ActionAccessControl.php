5f85e5d69bfc66ad8076addf1f77bdc65b7fbc43
Vitaly Voskobovich <vitali.voskobovich@moonion.com>
2015-12-14 14:06:41 +0200

    Dev

---
+++ filters/ActionAccessControl.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace voskobovich\rest\base\filters;
+
+use Yii;
+use yii\web\UnauthorizedHttpException;
+
+
+/**
+ * Class ActionAccessControl
+ * @package voskobovich\rest\base\filters
+ */
+class ActionAccessControl
+{
+    /**
+     * @param $permName
+     * @param $params
+     * @throws UnauthorizedHttpException
+     */
+    public static function checkAccess($permName, $params)
+    {
+        if (!Yii::$app->user->can($permName, $params)) {
+            throw new UnauthorizedHttpException('You are requesting with an invalid credential.');
+        }
+    }
+}
