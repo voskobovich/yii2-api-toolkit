26ff4385cb550bc2e7364ee90bb8c59d2b4d718b
voskobovich <vitaly@voskobovich.com>
2016-07-09 20:36:53 +0300

    Dev

--- src/filters/auth/QueryParamAuth.php
+++ src/filters/auth/QueryParamAuth.php
@@ -13,39 +13,12 @@ use yii\web\UnauthorizedHttpException;
 class QueryParamAuth extends \yii\filters\auth\QueryParamAuth
 {
     /**
-     * Actions ids
-     * @var array
-     */
-    public $exceptActions = [];
-
-    /**
-     * @inheritdoc
-     */
-    public function beforeAction($action)
-    {
-        if (in_array($action->id, $this->exceptActions)) {
-            return true;
-        }
-
-        $response = $this->response ?: Yii::$app->getResponse();
-
-        $this->authenticate(
-            $this->user ?: Yii::$app->getUser(),
-            $this->request ?: Yii::$app->getRequest(),
-            $response
-        );
-
-        return true;
-    }
-
-    /**
      * @inheritdoc
      */
     public function authenticate($user, $request, $response)
     {
-        $accessToken = $request->get($this->tokenParam);
-
-        if (!is_string($accessToken)) {
+        $accessToken = $request->get($this->tokenParam, null);
+        if ($accessToken == null) {
             throw new UnauthorizedHttpException('You are requesting with an invalid credential.');
         }
 
